add72731eb22bb8d858eab2bb596acdcaf23f3a6
Jin <o90744414aya@yahoo.co.jp>
2020-01-16 11:54:40 +0000

    php02

---
+++ practice.php
@@ -0,0 +1,12 @@
+<?php
+//echo 'hello php!';
+// 
+// $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月"];
+// // echo $array_month[7];
+// $hello = "Hello,";
+// $name = "Jin";
+// $world = "'s world!";
+// echo $hello.$name.$world;
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo $tech_boost;
